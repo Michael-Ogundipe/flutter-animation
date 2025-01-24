02d13c36d0f1edcdc650ff1972862e39e390eeb8
Ogundipe Michael <ogundipeibukun51@gmail.com>
2025-01-24 09:16:55 +0100

    example 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' show pi;
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -66,6 +68,10 @@ class HalfCircleClipper extends CustomClipper<Path> {
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
 }
 
+extension on VoidCallback {
+  Future<void> delayed(Duration duration) => Future.delayed(duration, this);
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -75,33 +81,158 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late AnimationController _counterClockWiseRotationController;
+  late Animation<double> _counterClockWiseRotationAnimation;
+
+  late AnimationController _flipController;
+  late Animation<double> _flipAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _counterClockWiseRotationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _counterClockWiseRotationAnimation = Tween<double>(
+      begin: 0,
+      end: -(pi / 2),
+    ).animate(
+      CurvedAnimation(
+        parent: _counterClockWiseRotationController,
+        curve: Curves.bounceOut,
+      ),
+    );
+
+    // flip animation
+    _flipController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _flipAnimation = Tween<double>(
+      begin: 0,
+      end: pi,
+    ).animate(CurvedAnimation(
+      parent: _flipController,
+      curve: Curves.bounceOut,
+    ));
+
+    // status listeners
+
+    _counterClockWiseRotationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _flipAnimation = Tween<double>(
+          begin: _flipAnimation.value,
+          end: _flipAnimation.value + pi,
+        ).animate(
+          CurvedAnimation(
+            parent: _flipController,
+            curve: Curves.bounceOut,
+          ),
+        );
+
+        // reset the flip controller and start the animation
+
+        _flipController
+          ..reset()
+          ..forward();
+      }
+    });
+
+    _flipController.addStatusListener(
+      (status) {
+        if (status == AnimationStatus.completed) {
+          _counterClockWiseRotationAnimation = Tween<double>(
+            begin: _counterClockWiseRotationAnimation.value,
+            end: _counterClockWiseRotationAnimation.value+ -(pi / 2),
+          ).animate(
+            CurvedAnimation(
+              parent: _counterClockWiseRotationController,
+              curve: Curves.bounceOut,
+            ),
+          );
+
+          _counterClockWiseRotationController..reset()..forward();
+        }
+
+
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _counterClockWiseRotationController.dispose();
+    _flipController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _counterClockWiseRotationController
+      ..reset()
+      ..forward.delayed(const Duration(seconds: 1));
+
     return Scaffold(
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipPath(
-              clipper: const HalfCircleClipper(side: CircleSide.left),
-              child: Container(
-                width: 150,
-                height: 150,
-                color: Colors.blue,
-              ),
-            ),
-            ClipPath(
-              clipper: const HalfCircleClipper(side: CircleSide.right),
-              child: Container(
-                width: 150,
-                height: 150,
-                color: Colors.yellow,
-              ),
-            )
-          ],
-        ),
+        child: AnimatedBuilder(
+            animation: _counterClockWiseRotationController,
+            builder: (context, child) {
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..rotateZ(_counterClockWiseRotationAnimation.value),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AnimatedBuilder(
+                        animation: _flipAnimation,
+                        builder: (context, child) {
+                          return Transform(
+                            alignment: Alignment.centerRight,
+                            transform: Matrix4.identity()
+                              ..rotateY(
+                                _flipAnimation.value,
+                              ),
+                            child: ClipPath(
+                              clipper: const HalfCircleClipper(
+                                  side: CircleSide.left),
+                              child: Container(
+                                width: 150,
+                                height: 150,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          );
+                        }),
+                    AnimatedBuilder(
+                        animation: _flipAnimation,
+                        builder: (context, child) {
+                          return Transform(
+                            alignment: Alignment.centerLeft,
+                            transform: Matrix4.identity()
+                              ..rotateY(
+                                _flipAnimation.value,
+                              ),
+                            child: ClipPath(
+                              clipper: const HalfCircleClipper(
+                                  side: CircleSide.right),
+                              child: Container(
+                                width: 150,
+                                height: 150,
+                                color: Colors.yellow,
+                              ),
+                            ),
+                          );
+                        })
+                  ],
+                ),
+              );
+            }),
       ),
     );
   }
